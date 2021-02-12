0dd663e79348c5ca07c019f1e7bc2e96c9d5b6b8
YannNunes <62283465+YannNunes@users.noreply.github.com>
2021-02-12 23:00:38 +0100

    Update SABR.py

--- SABR.py
+++ SABR.py
@@ -1,57 +1,66 @@
 import xlrd
 import math
-import numpy
+import numpy as np
+from scipy.stats import norm
 from scipy.optimize import minimize
 
 
-######## functions definitions ###############################################
+def formula(alpha, beta, nu, rho, v, time, logFK):
+    A = 1 + (((1 - beta) ** 2 * alpha ** 2) / (24. * (v ** 2)) +
+             (alpha * beta * nu * rho) / (4. * v) + ((nu ** 2) * (2 - 3 * (rho ** 2)) / 24.)) * time
+    B = 1 + (1 / 24.) * (((1 - beta) * logFK) ** 2) + (1 / 1920.) * (((1 - beta) * logFK) ** 4)
+    return A, B
 
 
-def SABR(alpha, beta, rho, nu, F, K, time, MKT):  # all variables are scalars
+def formula_vect(par, v, time, logFK):
+    A = 1 + (((1 - par[1]) ** 2 * par[0] ** 2) / (24. * (v ** 2)) + (par[0] * par[1] * par[3] * par[2]) / (
+            4. * v) + ((par[3] ** 2) * (2 - 3 * (par[2] ** 2)) / 24.)) * time
+    B = 1 + (1 / 24.) * (((1 - par[1]) * logFK) ** 2) + (1 / 1920.) * (((1 - par[1]) * logFK) ** 4)
+    return A, B
 
-    if K <= 0:  # negative rates' problem, need to shift the smile
-        VOL = 0
+
+def SABR(alpha, beta, rho, nu, forward, strike, time, market_vol):
+    if strike <= 0:  # the strike become negative so the smile need to be shifted.
+        vol = 0
         diff = 0
-    elif F == K:  # ATM formula
-        V = (F * K) ** ((1 - beta) / 2.)
-        logFK = math.log(F / K)
-        A = 1 + (((1 - beta) ** 2 * alpha ** 2) / (24. * (V ** 2)) + (alpha * beta * nu * rho) / (4. * V) + (
-                    (nu ** 2) * (2 - 3 * (rho ** 2)) / 24.)) * time
-        B = 1 + (1 / 24.) * (((1 - beta) * logFK) ** 2) + (1 / 1920.) * (((1 - beta) * logFK) ** 4)
-        VOL = (alpha / V) * A
-        diff = VOL - MKT
-    elif F != K:  # not-ATM formula
-        V = (F * K) ** ((1 - beta) / 2.)
-        logFK = math.log(F / K)
-        z = (nu / alpha) * V * logFK
+    elif forward == strike:  # ATM formula
+        v = (forward * strike) ** ((1 - beta) / 2.)
+        logFK = math.log(forward / strike)
+        a, b = formula(alpha, beta, nu, rho, v, time, logFK)
+        vol = (alpha / v) * a
+        diff = vol - market_vol
+    elif forward != strike:  # not-ATM formula
+        v = (forward * strike) ** ((1 - beta) / 2.)
+        logFK = math.log(forward / strike)
+        z = (nu / alpha) * v * logFK
         x = math.log((math.sqrt(1 - 2 * rho * z + z ** 2) + z - rho) / (1 - rho))
-        A = 1 + (((1 - beta) ** 2 * alpha ** 2) / (24. * (V ** 2)) + (alpha * beta * nu * rho) / (4. * V) + (
-                    (nu ** 2) * (2 - 3 * (rho ** 2)) / 24.)) * time
-        B = 1 + (1 / 24.) * (((1 - beta) * logFK) ** 2) + (1 / 1920.) * (((1 - beta) * logFK) ** 4)
-        VOL = (nu * logFK * A) / (x * B)
-        diff = VOL - MKT
+        a, b = formula(alpha, beta, nu, rho, v, time, logFK)
+        vol = (nu * logFK * a) / (x * b)
+        diff = vol - market_vol
 
-    print(round(VOL, 4), '\t', )
-    outvol.write('%r;' % round(VOL, 4))
-    if MKT == 0:
+    print(round(vol, 4), '\t', )
+    outvol.write('%r;' % round(vol, 4))
+    if market_vol == 0:
         diff = 0
         vol_diff.write('%s;' % 'No market data')
     else:
         vol_diff.write('%r;' % round(diff, 4))
 
 
-def smile(alpha, beta, rho, nu, F, K, time, MKT,
-          i):  # F, time and the parameters are scalars, K and MKT are vectors, i is the index for tenor/expiry label
+def smile(alpha, beta, rho, nu, forward, strike, time, market_vol, i):
+    # forward, time and the parameters are scalars,
+    # strike and market_vol are vectors,
+    # i is the index for tenor/expiry label
 
     print(label_ten[i], '\t', label_exp[i], '\t')
     outvol.write('%s;%s;' % (label_ten[i], label_exp[i]))
     vol_diff.write('%s;%s;' % (label_ten[i], label_exp[i]))
     parameters.write('%s;%s;' % (label_ten[i], label_exp[i]))
 
-    for j in range(len(K)):
-        if K[0] <= 0:
-            shift(F, K)
-        SABR(alpha, beta, rho, nu, F, K[j], time, MKT[j])
+    for j in range(len(strike)):
+        if strike[0] <= 0:
+            shift(forward, strike)
+        SABR(alpha, beta, rho, nu, forward, strike[j], time, market_vol[j])
 
     print(' ')
     outvol.write('\n')
@@ -60,14 +69,15 @@ def smile(alpha, beta, rho, nu, F, K, time, MKT,
     parameters.write('\n')
 
 
-def SABR_vol_matrix(alpha, beta, rho, nu, F, K, time,
-                    MKT):  # F, time and the parameters are vectors, K and MKT are matrices
+def SABR_vol_matrix(alpha, beta, rho, nu, forward, strike, time, market_vol):
+    # forward, time and the parameters are vectors,
+    # strike and market_vol are matrices
 
     print(' ')
-    print((2 + ((num_strikes - 1) / 2)) , '       ' , 'SABR VOLATILITIES')
-    print('  ', '\t', 'strikes:', )
+    print((2 + ((num_strikes - 1) / 2)), '       ', 'SABR VOLATILITIES')
+    print('  ', '\t', 'strikes:')
     for i in range(num_strikes):
-        print(label_strikes[i], '\t', )
+        print(label_strikes[i], '\t')
     print(' ')
     outvol.write('%s;' % 'SABR VOLATILITIES')
     outvol.write('\n')
@@ -86,57 +96,84 @@ def SABR_vol_matrix(alpha, beta, rho, nu, F, K, time,
     parameters.write('%s;%s;%s;%s;%s;%s' % ('tenor', 'expiry', 'alpha', 'beta', 'rho', 'nu'))
     parameters.write('\n')
 
-    for i in range(len(F)):
-        smile(alpha[i], beta[i], rho[i], nu[i], F[i], K[i], time[i], MKT[i], i)
+    for i in range(len(forward)):
+        smile(alpha[i], beta[i], rho[i], nu[i], forward[i], strike[i], time[i], market_vol[i], i)
 
 
-def shift(F, K):
-    shift = 0.001 - K[0]
-    for j in range(len(K)):
-        K[j] = K[j] + shift
-        F = F + shift
+def shift(forward, strike):
+    shift = 0.001 - strike[0]
+    for j in range(len(strike)):
+        strike[j] = strike[j] + shift
+        forward = forward + shift
 
 
-def objfunc(par, F, K, time, MKT):
+def objective(par, forward, strike, time, market_vol):
     sum_sq_diff = 0
-    if K[0] <= 0:
-        shift(F, K)
-    for j in range(len(K)):
-        if MKT[j] == 0:
+    if strike[0] <= 0:
+        shift(forward, strike)
+    for j in range(len(strike)):
+        if market_vol[j] == 0:
             diff = 0
-        elif F == K[j]:
-            V = (F * K[j]) ** ((1 - par[1]) / 2.)
-            logFK = math.log(F / K[j])
-            A = 1 + (((1 - par[1]) ** 2 * par[0] ** 2) / (24. * (V ** 2)) + (par[0] * par[1] * par[3] * par[2]) / (
-                        4. * V) + ((par[3] ** 2) * (2 - 3 * (par[2] ** 2)) / 24.)) * time
-            B = 1 + (1 / 24.) * (((1 - par[1]) * logFK) ** 2) + (1 / 1920.) * (((1 - par[1]) * logFK) ** 4)
-            VOL = (par[0] / V) * A
-            diff = VOL - MKT[j]
-        elif F != K[j]:
-            V = (F * K[j]) ** ((1 - par[1]) / 2.)
-            logFK = math.log(F / K[j])
-            z = (par[3] / par[0]) * V * logFK
+        elif forward == strike[j]:
+            v = (forward * strike[j]) ** ((1 - par[1]) / 2.)
+            logFK = math.log(forward / strike[j])
+            a, b = formula_vect(par, v, time, logFK)
+            vol = (par[0] / v) * a
+            diff = vol - market_vol[j]
+        elif forward != strike[j]:
+            v = (forward * strike[j]) ** ((1 - par[1]) / 2.)
+            logFK = math.log(forward / strike[j])
+            z = (par[3] / par[0]) * v * logFK
             x = math.log((math.sqrt(1 - 2 * par[2] * z + z ** 2) + z - par[2]) / (1 - par[2]))
-            A = 1 + (((1 - par[1]) ** 2 * par[0] ** 2) / (24. * (V ** 2)) + (par[0] * par[1] * par[3] * par[2]) / (
-                        4. * V) + ((par[3] ** 2) * (2 - 3 * (par[2] ** 2)) / 24.)) * time
-            B = 1 + (1 / 24.) * (((1 - par[1]) * logFK) ** 2) + (1 / 1920.) * (((1 - par[1]) * logFK) ** 4)
-            VOL = (par[3] * logFK * A) / (x * B)
-            diff = VOL - MKT[j]
+            a, b = formula_vect(par, v, time, logFK)
+            vol = (par[3] * logFK * a) / (x * b)
+            diff = vol - market_vol[j]
         sum_sq_diff = sum_sq_diff + diff ** 2
         obj = math.sqrt(sum_sq_diff)
     return obj
 
 
-def calibration(starting_par, F, K, time, MKT):
-    for i in range(len(F)):
+def calibration(starting_par, forward, strike, time, market_vol):
+    for i in range(len(forward)):
         x0 = starting_par
         bnds = ((0.001, None), (0, 1), (-0.999, 0.999), (0.001, None))
-        res = minimize(objfunc, x0, (F[i], K[i], time[i], MKT[i]), bounds=bnds,
-                       method='SLSQP')  # for a constrained minimization of multivariate scalar functions
+        res = minimize(objective, x0, (forward[i], strike[i], time[i], market_vol[i]), bounds=bnds, method='SLSQP')
+        # for a constrained minimization of multivariate scalar functions
         alpha[i] = res.x[0]
         beta[i] = res.x[1]
         rho[i] = res.x[2]
         nu[i] = res.x[3]
+
+
+def black_scholes(forward, strike, spot, time, market_vol, callorput):
+    d1 = (np.log(spot / K) + (forward + 0.5 * market_vol ** 2) * time) / (market_vol * np.sqrt(time))
+    d2 = d1() - market_vol * np.sqrt(time)
+
+    if callorput == "call":
+        return spot * norm.cdf(d1()) - K * np.exp(-forward * time) * \
+               norm.cdf(d2())
+
+    elif calorput == "put":
+        return -spot * norm.cdf(-d1()) + strike * np.exp(-forward * time) * \
+               norm.cdf(-d2())
+
+
+def black_scholes_matrix(forward, strike, spot, time, market_vol, callorput):
+    print(' ')
+    print((2 + ((num_strikes - 1) / 2)), '       ', 'SABR Price')
+    print('  ', '\t', 'Price:')
+    for i in range(num_strikes):
+        print(label_strikes[i], '\t')
+    print(' ')
+    price.write('%s;' % 'Price')
+    price.write('\n')
+    price.write('%s;%s;' % (' ', 'strikes:'))
+    for j in range(len(strike_spreads)):
+        price.write('%s;' % label_strikes[j])
+    price.write('\n')
+    print('tenor', '\t', 'expiry')
+    for i in range(len(forward)):
+        black_scholes(forward[i], strike[i],spot[i], time[i], market_vol[i], callorPut[i])
 
 
 ################################################################################################################################################
@@ -147,6 +184,7 @@ def calibration(starting_par, F, K, time, MKT):
 outvol = open('outvol.csv', 'w')  # file output of volatilities
 vol_diff = open('vol differences.csv', 'w')  # file output differences between SABR and Market volatilities
 parameters = open('parameters.csv', 'w')  # file output parameters
+price = open('price.csv', 'w')  # file output price
 
 while True:
     try:
@@ -197,19 +235,19 @@ while True:
         break
 
 # to create the strike grid
-K = numpy.zeros((len(F), num_strikes))
+K = np.zeros((len(F), num_strikes))
 for i in range(len(F)):
     for j in range(num_strikes):
         K[i][j] = F[i] + 0.0001 * (strike_spreads[j])
 
     # to create market volatilities
-MKT = numpy.zeros((len(F), num_strikes))
+MKT = np.zeros((len(F), num_strikes))
 for i in range(len(F)):
     for j in range(num_strikes):
         MKT[i][j] = Market_data.cell(2 + i, 3 + j).value
 
 # set starting parameters
-starting_guess = numpy.array([0.001, 0.5, 0, 0.001])
+starting_guess = np.array([0.001, 0.5, 0, 0.001])
 alpha = len(F) * [starting_guess[0]]
 beta = len(F) * [starting_guess[1]]
 rho = len(F) * [starting_guess[2]]
