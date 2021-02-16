e15b25d2d7b28cba57b20eddef03c97cebcb3ae2
Gabin Salabert <gabin.salabert2@laposte.net>
2021-02-17 00:06:37 +0100

    For loop fixed + import added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from SABR_function import *
 from input import*
+from openpyxl import workbook
+from openpyxl.cell import get_column_letter
 
 time, tenor = maturite(dfvols)
 strike = swaprates
@@ -13,20 +15,20 @@ prices_put = bachelier_matrix(matrix_vol, spot, strike, f, time, "put")
 
 wb = openpyxl.load_workbook('Option_3_DataSet.xls')
 sheet = wb.get_sheet_by_name('Option_3_DataSet')
-for data in enumerate(array):
-    worksheet.write_column(2, 9, prices_call)
-for data in enumerate(array):
-    worksheet.write_column(2, 10, prices_put)
-for data in enumerate(array):
-    worksheet.write_column(2, 11, time)
-for data in enumerate(array):
-    worksheet.write_column(2, 12, tenor)
-for data in enumerate(array):
-    worksheet.write_column(2, 13, strike)
-for data in enumerate(array):
-    worksheet.write_column(2, 14, MKT)
-for data in enumerate(array):
-    worksheet.write_column(2, 15, spot)
+for data in enumerate(prices_call):
+    worksheet.write_column(2, 9, data)
+for data in enumerate(prices_put):
+    worksheet.write_column(2, 10, data)
+for data in enumerate(time):
+    worksheet.write_column(2, 11, data)
+for data in enumerate(tenor):
+    worksheet.write_column(2, 12, data)
+for data in enumerate(strike):
+    worksheet.write_column(2, 13, data)
+for data in enumerate(MKT):
+    worksheet.write_column(2, 14, data)
+for data in enumerate(spot):
+    worksheet.write_column(2, 15, data)
     
 sheet.cell(row=(2),column=5).value = alpha
 sheet.cell(row=(2),column=6).value = beta
