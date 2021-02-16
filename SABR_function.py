2a744d59d9fa9ebf680957ed4368d7a5708ed471
YannNunes <62283465+YannNunes@users.noreply.github.com>
2021-02-16 16:31:14 +0100

    Update SABR_function.py

--- SABR_function.py
+++ SABR_function.py
@@ -1,8 +1,8 @@
-from set_label import *
 import math
 import scipy.stats as si
 from scipy.optimize import minimize
 import sympy as sy
+from set_swaption import*
 
 def formula(alpha, beta, nu, rho, v, time, logFK):
     A = 1 + (((1 - beta) ** 2 * alpha ** 2) / (24. * (v ** 2)) +
@@ -10,83 +10,36 @@ def formula(alpha, beta, nu, rho, v, time, logFK):
     B = 1 + (1 / 24.) * (((1 - beta) * logFK) ** 2) + (1 / 1920.) * (((1 - beta) * logFK) ** 4)
     return A, B
 
-def forwardrates(annuities, swaps):
-    
 
-
-def SABR(alpha: float, beta: float, rho: float, nu: float, forward: float , strike: float, time: float, market_vol: int):
+def SABR(alpha: float, beta: float, rho: float, nu: float, forward: float , strike: float, time: float):
 
     logFK = math.log(forward / strike)
     v = (forward * strike) ** ((1 - beta) / 2.)
     a, b = formula(alpha, beta, nu, rho, v, time, logFK)
     if strike <= 0:  # the strike become negative so the smile need to be shifted.
         vol = 0
-        diff = 0
     elif forward == strike:  # ATM formula
         vol = (alpha / v) * a
-        diff = vol - market_vol
     elif forward != strike:  # not-ATM formula
         z = (nu / alpha) * v * logFK
         x = math.log((math.sqrt(1 - 2 * rho * z + z ** 2) + z - rho) / (1 - rho))
         vol = (nu * logFK * a) / (x * b)
-        diff = vol - market_vol
-
-    print(round(vol, 4), '\t', )
-    outvol.write('%r;' % round(vol, 4))
-    if market_vol == 0:
-        diff = 0
-        vol_diff.write('%s;' % 'No market data')
-    else:
-        vol_diff.write('%r;' % round(diff, 4))
+    return vol
 
 
 def smile(alpha: float, beta: float, rho: float, nu: float, forward: float , strike: list, time: float, market_vol: list, i:int):
-
-    print(label_ten[i], '\t', label_exp[i], '\t')
-    outvol.write('%s;%s;' % (label_ten[i], label_exp[i]))
-    vol_diff.write('%s;%s;' % (label_ten[i], label_exp[i]))
-    parameters.write('%s;%s;' % (label_ten[i], label_exp[i]))
-
+    MKT=()
     for j in range(len(strike)):
         if strike[0] <= 0:
             shift(forward, strike)
-        SABR(alpha, beta, rho, nu, forward, strike[j], time, market_vol[j])
-
-    print(' ')
-    outvol.write('\n')
-    vol_diff.write('\n')
-    parameters.write('%f;%f;%f;%f;' % (alpha, beta, rho, nu))
-    parameters.write('\n')
-
+        MKT.append(SABR(alpha, beta, rho, nu, forward, strike[j], time, market_vol[j]))
+    return MKT
 
 def SABR_vol_matrix(alpha: list, beta: list, rho: list, nu: list, forward: list, strike: bytearray, time: list, market_vol: bytearray):
-
-    print(' ')
-    print((2 + ((num_strikes - 1) / 2)), '       ', 'SABR VOLATILITIES')
-    print('  ', '\t', 'strikes:')
-    for i in range(num_strikes):
-        print(label_strikes[i], '\t')
-    print(' ')
-    outvol.write('%s;' % 'SABR VOLATILITIES')
-    outvol.write('\n')
-    vol_diff.write('%s;' % 'VOLATILITY DIFFERENCES')
-    vol_diff.write('\n')
-    parameters.write('%s;' % 'PARAMETERS')
-    parameters.write('\n')
-    outvol.write('%s;%s;' % (' ', 'strikes:'))
-    vol_diff.write('%s;%s;' % (' ', 'strikes:'))
-    for j in range(len(strike_spreads)):
-        outvol.write('%s;' % label_strikes[j])
-        vol_diff.write('%s;' % label_strikes[j])
-    outvol.write('\n')
-    vol_diff.write('\n')
-    print('tenor', '\t', 'expiry')
-    parameters.write('%s;%s;%s;%s;%s;%s' % ('tenor', 'expiry', 'alpha', 'beta', 'rho', 'nu'))
-    parameters.write('\n')
-
+    MKT=[]
     for i in range(len(forward)):
-        smile(alpha[i], beta[i], rho[i], nu[i], forward[i], strike[i], time[i], market_vol[i], i)
-
+        MKT.append(smile(alpha[i], beta[i], rho[i], nu[i], forward[i], strike[i], time[i], market_vol[i], i))
+    return MKT
 
 def shift(forward, strike):
     shift = 0.001 - strike[0]
@@ -144,19 +97,6 @@ def bachelier(market_vol, spot, strike, forward, time, option):
 
 
 def bachelier_matrix(forward, strike, spot, time, market_vol, option):
-    print(' ')
-    print((2 + ((num_strikes - 1) / 2)), '       ', 'SABR Price')
-    print('  ', '\t', 'Price:')
-    for i in range(num_strikes):
-        print(label_strikes[i], '\t')
-    print(' ')
-    price.write('%s;' % 'Price')
-    price.write('\n')
-    price.write('%s;%s;' % (' ', 'strikes:'))
-    for j in range(len(strike_spreads)):
-        price.write('%s;' % label_strikes[j])
-    price.write('\n')
-    print('tenor', '\t', 'expiry')
     for i in range(len(forward)):
         bachelier(forward[i], strike[i], spot[i], time[i], market_vol[i], option[i])
 
@@ -196,23 +136,6 @@ def vega(forward, strike, spot, time, market_vol):
     return spot * si.norm.pdf(d1(forward, strike, spot, time, market_vol)) * np.sqrt(time)
 
 
-def maturite(df):
-    time=[]
-    for col in df.columns:
-        if col[0:2]=="0A":
-            time.append(1/12)
-        elif col[0:2]=="0C":
-            time.append(3/12)
-        elif col[0:2]=="0F":
-            time.append(6/12)
-        elif col[0:2]=="0I":
-            time.append(9/12)
-        else:
-            time.append(int(col[0:2]))
-    return time
-
-
-
 def rho(forward, strike, spot, time, market_vol, option):
     if option == 'call':
         rho = time * strike * np.exp(-forward * time) * si.norm.cdf(d2(forward, strike, spot, time, market_vol), 0.0,
@@ -233,3 +156,18 @@ def volga(forward, strike, spot, time, market_vol):
     return np.sqrt(time) * si.norm.pdf(d1(forward, strike, spot, time, market_vol)) * \
            (d1(forward, strike, spot, time, market_vol) *
             d2(forward, strike, spot, time, market_vol)) / market_vol
+
+def maturite(df):
+    time=[]
+    for col in df.columns:
+        if col[0:2]=="0A":
+            time.append(1/12)
+        elif col[0:2]=="0C":
+            time.append(3/12)
+        elif col[0:2]=="0F":
+            time.append(6/12)
+        elif col[0:2]=="0I":
+            time.append(9/12)
+        else:
+            time.append(int(col[0:2]))
+    return time
