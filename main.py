492249969fa1c67404ef9fe1ab78bfc19ead85dc
Gabin Salabert <gabin.salabert2@laposte.net>
2021-02-16 23:57:12 +0100

    Display fixed for arrays, excel sheet edited

--- main.py
+++ main.py
@@ -11,17 +11,26 @@ matrix_vol = SABR_vol_matrix(alpha, beta, rho, nu, f, strike, time)
 prices_call = bachelier_matrix(matrix_vol, spot, strike, f, time, "call")
 prices_put = bachelier_matrix(matrix_vol, spot, strike, f, time, "put")
 
-wb = openpyxl.load_workbook('queue.xls')
-sheet = wb.get_sheet_by_name('Sheet1')
-sheet.cell(row=(4),column=3).value = alpha
-sheet.cell(row=(5),column=3).value = beta
-sheet.cell(row=(6),column=3).value = rho
-sheet.cell(row=(7),column=3).value = nu
-sheet.cell(row=(8),column=3).value = prices_call
-sheet.cell(row=(9),column=3).value = prices_put
-sheet.cell(row=(10),column=3).value = time
-sheet.cell(row=(11),column=3).value = tenor
-sheet.cell(row=(12),column=3).value = strike
-sheet.cell(row=(13),column=3).value = MKT
-sheet.cell(row=(14),column=3).value = spot
-wb.save('queue.xls')
+wb = openpyxl.load_workbook('Option_3_DataSet.xls')
+sheet = wb.get_sheet_by_name('Option_3_DataSet')
+for data in enumerate(array):
+    worksheet.write_column(2, 9, prices_call)
+for data in enumerate(array):
+    worksheet.write_column(2, 10, prices_put)
+for data in enumerate(array):
+    worksheet.write_column(2, 11, time)
+for data in enumerate(array):
+    worksheet.write_column(2, 12, tenor)
+for data in enumerate(array):
+    worksheet.write_column(2, 13, strike)
+for data in enumerate(array):
+    worksheet.write_column(2, 14, MKT)
+for data in enumerate(array):
+    worksheet.write_column(2, 15, spot)
+    
+sheet.cell(row=(2),column=5).value = alpha
+sheet.cell(row=(2),column=6).value = beta
+sheet.cell(row=(2),column=7).value = rho
+sheet.cell(row=(2),column=8).value = nu
+
+wb.save('Option_3_DataSet.xls')
